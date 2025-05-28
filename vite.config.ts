f96f4ab24bf5a45de1565758521625247df18c13
Kei <keiroku13425@gmail.com>
2025-05-28 18:58:38 -0400

    FFF

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/Rhiley/' : '/',
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            { src: 'public/pictures', dest: 'docs' }  // public/pictures → docs/pictures にコピー
+          ],
+          hook: 'writeBundle' // 書き出し完了後に実行
+        })
+      ]
+    }
   },
   plugins: [react()],
 }));
