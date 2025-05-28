89b916698773a1dc905cfd3f53c375d7c0169e99
Kei <keiroku13425@gmail.com>
2025-05-28 11:40:25 -0400

    Build for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rhiley.profile/', // ← GitHub Pages のパスを追加
+  build: {
+    outDir: 'docs', // ← 出力先を docs に（GitHub Pages 用）
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
