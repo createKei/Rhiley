5f8ab9ea561b94db3cda9b1df5d617ca1c30d55e
Kei <keiroku13425@gmail.com>
2025-05-29 13:33:46 -0400

    Switch images

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -142,7 +142,7 @@ const HomePage: React.FC = () => {
               },
               {
                 title: "Family",
-                img: `${import.meta.env.BASE_URL}pictures/familyportrait.jpg`,
+                img: `${base}pictures/familyportrait.jpg`,
                 description: "Preserving family moments and connections for generations."
               }
             ].map((service, index) => (
